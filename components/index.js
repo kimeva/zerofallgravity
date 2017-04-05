cfb9ee386461dd543bef0f88346f8752200233ca
Kim Evangelista <kime924.dev@live.com.sg>
2017-04-05 17:23:28 +0800

    Added favicons.

--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { Page } from './Page/Page';
 export { ImageLoader } from './ImageLoader/ImageLoader';
 export { Navbar } from './Navbar/Navbar';
+export { HeadComponent } from './HeadComponent/HeadComponent';
