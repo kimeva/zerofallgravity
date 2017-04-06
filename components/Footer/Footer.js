044fb48712c7ad9a985bb48487b3d76f6f5b870c
Kim Evangelista <kime924.dev@live.com.sg>
2017-04-06 09:31:39 +0800

    Updated footer and navbar. Added a “coming-soon” page.

---
+++ components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import styles from './Footer.scss';
+
+export class Footer extends Component {
+  render() {
+    const href = '/';
+    return (
+      <div className="footer-container">
+        <Head>
+          <link rel="stylesheet" href="static/styles/font-awesome/css/font-awesome.min.css"/>
+        </Head>
+        <style dangerouslySetInnerHTML={{ __html: styles }} />
+        <ul className="footer-links">
+          <li>
+            <a target="_blank" href="https://www.facebook.com/zerofallgravity/">
+              <i className="social-media-icon fa fa-facebook-official" aria-hidden="true"></i>
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="https://www.instagram.com/zerofallgravity/">
+              <i className="social-media-icon fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { Page } from './Page/Page';
 export { ImageLoader } from './ImageLoader/ImageLoader';
 export { Navbar } from './Navbar/Navbar';
 export { HeadComponent } from './HeadComponent/HeadComponent';
+export { Footer } from './Footer/Footer';
